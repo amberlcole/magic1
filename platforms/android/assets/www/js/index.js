c4548381f3748a7d1339e98d1bc6d43f74e8f2b3
amberlcole <amber.l.cole@asu.edu>
2016-03-20 20:30:41 -0700

    The Magic App
    Apache Cordova Application for 402 Capstone

---
+++ platforms/android/assets/www/js/index.js
@@ -0,0 +1,48 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:inline-block;');
+
+        console.log('Received Event: ' + id);
+    }
+	
+	$(idescription).on("tap",function(){
+		$(this).show();
+	});
+	
+	//function showInfo() {
+	//	document.getElementById("idescription").style.display = "inline-block";
+	//}
+	
+	//Here's where my code startsssssssssssssssssssssssssssssssssssss
+	public function navigate(current,next){
+		document.getElementById(current).style.display = "none";
+		document.getElementById(current).style.display = "inline-block";
+	}
+};
+
+app.initialize();
