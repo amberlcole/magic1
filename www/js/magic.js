53ad99495bb86726c259a33ee8f7ac0f649ef69d
amberlcole <amber.l.cole@asu.edu>
2016-03-23 18:06:56 -0700

    more js
    fancy

--- www/js/magic.js
+++ www/js/magic.js
@@ -1,10 +1,29 @@
-//Here's where my code startsssssssssssssssssssssssssssssssssssss
-
-
 $(document).ready(function (){
+
+/************Page 1 Javascript************/	
+	//Itinerary button actions
+	$("#cbutton").on("tap", function(){
+		var cbit = 1;
+		navigation( page1,page2 );
+	});
+	
+	$("#rbutton").on("tap", function(){
+		navigation( page1,page2 );
+	});
+	
+	//Description button action
     $("#ibutton").on("tap",function(){
 		$("#idescription").toggle();
 	}); 
+	
+/************Page 2 Javascript************/	
+
+	
 });
+
+function navigation(c,n){
+	$( c ).hide();
+	$( n ).show();
+};
 
 
